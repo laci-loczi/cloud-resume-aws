5c838f55921ada8086ce4fc252fc94e4f4f36a45
Lóczi László <loczilaci0121@gmail.com>
2026-02-17 14:23:47 +0100

    changing to cloudflare tunnel again again

--- js/cluster.js
+++ js/cluster.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const BACKEND_URL = "https://www.k8s.loczidity.me"; 
+    const BACKEND_URL = "https://k8s.loczidity.me/"; 
     const btn = document.getElementById('tunnelButton');
     const dot = btn ? btn.querySelector('.dot') : null;
 
